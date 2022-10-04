d1bef4a3712ffb8fad131edc602bbcb40d083b74
Le Minh Tri <minhtri0795@gmail.com>
2022-10-04 16:13:58 +0700

    update test endpoint

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ import {
 } from "@apollo/client";
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: url || "http://newheadless.local/graphql",
+    uri: "http://newheadless.local/graphql",
     fetch,
   }),
   cache: new InMemoryCache(),
